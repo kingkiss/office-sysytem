78eb3bf6c9262326d74ef1b60282c8ff32351101
kissking <459531854@qq.com>
2018-12-21 00:10:56 +0800

    first commit

---
+++ src/main/java/com/office/manage/ManageSystemApplication.java
@@ -0,0 +1,14 @@
+package com.office.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageSystemApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/office/manage/ManageSystemApplicationTests.java
@@ -0,0 +1,17 @@
+package com.office.manage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManageSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
