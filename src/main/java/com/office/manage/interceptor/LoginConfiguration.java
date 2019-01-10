24a2cd8f03f1b4f1b074afa5ae635b0fd8b70b2a
kissking <459531854@qq.com>
2019-01-10 19:58:47 +0800

    实现了登陆拦截器interceptor

---
+++ src/main/java/com/office/manage/interceptor/LoginConfiguration.java
@@ -0,0 +1,29 @@
+package com.office.manage.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LoginConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// 注册拦截器
+        LoginInterceptor loginInterceptor = new LoginInterceptor();
+        InterceptorRegistration loginRegistry = registry.addInterceptor(loginInterceptor);
+        // 拦截路径
+        loginRegistry.addPathPatterns("/**");
+        // 排除路径
+        loginRegistry.excludePathPatterns("/");
+        loginRegistry.excludePathPatterns("/login");
+        loginRegistry.excludePathPatterns("/loginout");
+        loginRegistry.excludePathPatterns("/userLogin");
+        // 排除资源请求
+        loginRegistry.excludePathPatterns("/css/login/*.css");
+        loginRegistry.excludePathPatterns("/js/login/**/*.js");
+        loginRegistry.excludePathPatterns("/image/login/*.png");
+	}
+
+}
---
+++ src/main/java/com/office/manage/interceptor/LoginInterceptor.java
@@ -0,0 +1,40 @@
+package com.office.manage.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		Object user = session.getAttribute("user_id");
+		if(user != null){
+//			System.out.println("good");
+			return true;
+		}else{
+			response.sendRedirect(request.getContextPath()+"/login");
+//			System.out.println("error");
+			return false;
+		}
+		
+	}
+
+}
